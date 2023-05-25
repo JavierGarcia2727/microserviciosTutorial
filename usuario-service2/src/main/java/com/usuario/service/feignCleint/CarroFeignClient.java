264a8c31c006937ed66e1521d4ea9694ea4366cd
Javi <marvin_garcia27@hotmail.com>
2023-05-25 00:11:25 -0600

    config-server

--- usuario-service2/src/main/java/com/usuario/service/feignCleint/CarroFeignClient.java
+++ usuario-service2/src/main/java/com/usuario/service/feignCleint/CarroFeignClient.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import com.usuario.service.modelos.Carro;
 import com.usuario.service.modelos.Moto;
 
-@FeignClient(name="carro-service", url = "http://localhost:8081")
+@FeignClient(name="carro-service", url = "http://localhost:8083")
 public interface CarroFeignClient {
 	
 	@PostMapping("/carro/guardar")
